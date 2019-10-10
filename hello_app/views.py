b332cb81dc4515d2c0d25b797cbbb35acc6752bd
Michel Corne <mcorne@yahoo.com>
2019-10-10 12:59:45 +0200

    refactored app

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,9 +1,13 @@
-import re
 from datetime import datetime
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+from . import app
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 
 @app.route("/about/")
@@ -16,17 +20,12 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
-
-
 @app.route("/hello/")
 @app.route("/hello/<name>")
 def hello_there(name=None):
     return render_template("hello_there.html", name=name, date=datetime.now())
 
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
